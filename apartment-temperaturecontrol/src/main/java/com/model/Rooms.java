ec4f6628a82ed4422376ecd4ebc32e2386db02c8
jforjha <gauravjha0907@gmail.com>
2025-01-31 05:49:41 +0530

    Project setup with Maven and also added room class with temperature balanced logic

---
+++ apartment-temperaturecontrol/src/main/java/com/model/Rooms.java
@@ -0,0 +1,46 @@
+package com.model;
+
+import java.util.Random;
+
+public class Rooms {
+    private int roomId;
+    private double currentTemperature;
+    private boolean heatingFlag;
+    private boolean coolingFlag;
+
+    public Rooms(int roomId) {
+        this.roomId = roomId;
+        this.currentTemperature = new Random().nextDouble() * (40 - 10) + 10; 
+        this.heatingFlag = false;
+        this.coolingFlag = false;
+    }
+
+    public int getRoomId() {
+        return roomId;
+    }
+
+    public double getcurrentTemperature() {
+        return currentTemperature;
+    }
+
+    public boolean heatingFlag() {
+        return heatingFlag;
+    }
+
+    public boolean coolingFlag() {
+        return coolingFlag;
+    }
+
+    public void balanceTemperature(double requestedBuildingTemperature) {
+        if (currentTemperature < requestedBuildingTemperature) {
+            heatingFlag = true;
+            coolingFlag = false;
+        } else if (currentTemperature > requestedBuildingTemperature) {
+            heatingFlag = false;
+            coolingFlag = true;
+        } else {
+            heatingFlag = false;
+            coolingFlag = false;
+        }
+    }
+}
