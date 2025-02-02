ab67a83fec448faa05b4e73359120c7e6c4aab4c
Gaurav Jha <45852258+jforjha@users.noreply.github.com>
2025-02-02 17:14:02 +0530

    Added input for temperature setpoint adjustment

--- apartment-temperaturecontrol/src/main/java/com/model/Main.java
+++ apartment-temperaturecontrol/src/main/java/com/model/Main.java
@@ -1,16 +1,34 @@
 package com.model;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Building building = new Building();
-        System.out.println("Requested Building Temperature: " + building.getRequestedBuildingTemperature() + "degrees");
+        
+        while (true) {
         
         for (Rooms room : building.getRooms()) {
             System.out.println("Room ID: " + room.getRoomId() +
-                               ", Current Room Temperature: " + room.getcurrentTemperature() + "degrees" +
+                               ", Current Room Temperature: " + room.getcurrentTemperature() + " degrees" +
                                ", Heating Switch: " + (room.heatingFlag() ? "ON" : "OFF") +
                                ", Cooling Switch: " + (room.coolingFlag() ? "ON" : "OFF"));
         }
+        
+        System.out.print("kindly give the requested Building Temperature: ");
+        double userInputTemp = scanner.nextDouble();
+        if (userInputTemp < 10 ||  userInputTemp > 40)
+        	{
+        	 System.out.print("\nkindly give the requested Building Temperature between 10 to 40 as the default rooms temperature lies between the range of 10 to 40 degrees ");
+        	continue;
+        	}
+       
+        building.setRequestedBuildingTemperature(userInputTemp);
+        
+       
+    }
+        
     }
 
 }
